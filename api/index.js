a61be856ed35843e48dc7b2017230cc652b56d85
Nilesh Sangle <nileshsangle2003@gmail.com>
2024-02-18 17:09:57 +0530

    connected database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,33 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app=express()
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("server connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-app.get('/',(req,res)=>{
-    res.send({"status":"success"})
-})
+// async function main(){
+//     try {
+//         await mongoose.connect(process.env.MONGO)
+//         console.log("server connected")
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
-app.listen(3000,()=>{
-    console.log("server started")
-})
+// main().catch()
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ status: "success" });
+});
+
+app.listen(3000, () => {
+  console.log("server started");
+});
